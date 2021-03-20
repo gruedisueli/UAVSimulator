a1e7c497e66b9d972214b3ceaafaf1c164e8e168
gruedisueli <gruedisueli@certainmeasures.com>
2021-03-19 20:09:24 -0400

    Developing info panels
    addendum to previous commit.

---
+++ Assets/Scripts/UI/Tools/ParkingInfoPanel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+using Assets.Scripts.Environment;
+using Assets.Scripts.UI.EventArgs;
+
+namespace Assets.Scripts.UI.Tools
+{
+    public class ParkingInfoPanel : ElementInfoPanel
+    {
+        public override void Initialize(SceneElementBase sceneElement)
+        {
+            base.Initialize(sceneElement);
+
+            var sPark = sceneElement as SceneParkingStructure;
+            if (sPark == null)
+            {
+                Debug.LogError("Provided scene element for parking structure info panel is not a scene parking structure");
+                return;
+            }
+
+            var specs = sPark.ParkingStructureSpecs;
+            string t = specs.Type;
+            var asset = EnvironManager.Instance.ParkingStructAssets[t];
+            _image.sprite = asset.PreviewImage;
+
+            SetTextElement(ElementPropertyType.Type, t);
+            SetTextElement(ElementPropertyType.Description, specs.Description);
+            SetTextElement(ElementPropertyType.Rotation, specs.Rotation.y.ToString("F2"));
+        }
+
+        protected override void ModifyTextValues(IModifyElementArgs args)
+        {
+            try
+            {
+                switch (args.Update.Type)
+                {
+                    case ElementPropertyType.Rotation:
+                        {
+                            SetTextElement(ElementPropertyType.Rotation, (args.Update as ModifyFloatPropertyArg).Value.ToString("F2"));
+                            break;
+                        }
+                }
+
+            }
+            catch
+            {
+                Debug.LogError("Casting error in parking structure modify update");
+                return;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Tools/RestrictionInfoPanel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+using Assets.Scripts.Environment;
+
+namespace Assets.Scripts.UI.Tools
+{
+    public class RestrictionInfoPanel : ElementInfoPanel
+    {
+        public override void Initialize(SceneElementBase sceneElement)
+        {
+            base.Initialize(sceneElement);
+
+            var sRest = sceneElement as SceneRestrictionZone;
+            if (sRest == null)
+            {
+                Debug.LogError("Provided scene element for restrction zone info panel is not a scene restriction zone");
+                return;
+            }
+
+            var specs = sRest.RestrictionZoneSpecs;
+            string t = specs 
+
+                //different types of restriction zones mean params may be different from one to next.
+        }
+    }
+}
